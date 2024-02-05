423d53c02385a14e9fe0f3022a9f3490e35916b3
Administrator <rodionkireev47@yandex.ru>
2024-02-05 10:53:18 +0300

    pre-commit_OK

--- src/redis_test/redis_manage.py
+++ src/redis_test/redis_manage.py
@@ -3,11 +3,13 @@ import json
 import redis  # type: ignore
 from fastapi.encoders import jsonable_encoder
 
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+from src.config import REDIS_HOST, REDIS_PORT
+
+redis_client = redis.StrictRedis(host=f'{REDIS_HOST}', port=REDIS_PORT, db=0)
 
 
 class RedisTools:
-    redis_client: redis.StrictRedis = redis.StrictRedis(host='localhost', port=6379, db=0)
+    redis_client: redis.StrictRedis = redis.StrictRedis(host=f'{REDIS_HOST}', port=REDIS_PORT, db=0)
 
     def set_params(self, key, data):
         data_str = json.dumps(jsonable_encoder(data))
