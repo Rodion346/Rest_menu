f26b50e86a3aa6ad63541777acb94ba84337fd73
Administrator <rodionkireev47@yandex.ru>
2024-02-12 08:44:23 +0300

    final

--- docker/app.sh
+++ docker/app.sh
@@ -2,6 +2,7 @@
 
 sleep 10
 
+alembic downgrade head
 alembic upgrade head
 
 uvicorn main:app --reload --host=0.0.0.0 --port=8000
--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,7 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 
 from src.config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from src.db.database import Base
+from src.models.models import Menu
 
 sys.path.append(os.path.join(sys.path[0], 'src'))
 
@@ -30,7 +30,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
+target_metadata = Menu.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
